f326446ed1350d0dabf01d510ba8c9958f84c51a
noabenita <noaben2512@gmail.com>
2022-04-04 20:01:36 +0300

    update login and index

--- src/index.js
+++ src/index.js
@@ -21,12 +21,23 @@ import LoginPage from './loginPage/LoginPage';
 import reportWebVitals from './reportWebVitals';
 import SignUpPage from './signUpPage/SignUpPage';
 
+class User{
+  constructor(username, nickname, img, password, confirmpassword){
+    this.UserName = username;
+    this.Nickname = nickname;
+    this.Img = img;
+    this.Password = password;
+    this.ConfirmPassword = confirmpassword;
+  }
+}
+
+const dataStracture= [new User('or', 'orush', 'photo.jpg', 12345, 12345)];
 
 ReactDOM.render(
   <React.StrictMode>
       <BrowserRouter>
             <Routes>
-              <Route path="/" element ={<LoginPage />}>
+              <Route path="/" element ={<LoginPage db={dataStracture}/>}>
               </Route>
               <Route path="/SignUpPage" element ={<SignUpPage />}>
               </Route>
