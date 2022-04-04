ec1c26376a67d653fd9ecdfa49022f387167d34e
OrNasri <ornasri5@gmail.com>
2022-04-04 15:20:20 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import LoginPage from './loginPage/LoginPage';
+// import reportWebVitals from './reportWebVitals';
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <LoginPage />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import ChatPage from './chatPage/ChatPage';
+import LoginPage from './loginPage/LoginPage';
 import reportWebVitals from './reportWebVitals';
+import SignUpPage from './signUpPage/SignUpPage';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+            <Routes>
+              <Route path="/" element ={<LoginPage />}>
+              </Route>
+              <Route path="/SignUpPage" element ={<SignUpPage />}>
+              </Route>
+              <Route path="/ChatPage" element ={<ChatPage />}>
+              </Route>
+            </Routes>
+        </BrowserRouter>  
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
