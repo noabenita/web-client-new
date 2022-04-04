10d3aaac896fe1f0d089241443d8581c84d03720
noabenita <noaben2512@gmail.com>
2022-04-04 22:31:05 +0300

    update

--- src/loginPage/LoginPage.js
+++ src/loginPage/LoginPage.js
@@ -10,50 +10,51 @@ import ChatPage from '../chatPage/ChatPage';
 function LoginPage({db}) {
     const [usersArray,setUserArray] = useState({username:'', password:''});
     var nav = useNavigate();
-        function ifSubmit() {
-            if(db.find((e) => e.UserName == usersArray.username && e.Password == usersArray.password)) {
-                nav("/ChatPage");
-            } else {
-                alert('failed');
-            }
+    
+    function ifSubmit() {
+        if(db.find((e) => e.UserName == usersArray.username && e.Password == usersArray.password)) {
+            nav("/ChatPage");
+        } else {
+            alert('failed');
         }
+    }
         
-        function ifChange(event) {
-            const {name, value} = event.target;
-            setUserArray({
-                ...usersArray,
-                [name]:value
-            })
-        }
+    function ifChange(event) {
+        const {name, value} = event.target;
+        setUserArray({
+            ...usersArray,
+            [name]:value
+        })
+    }
         
-        return ( 
-            <>  
-            <form onSubmit={ifSubmit}> 
-                <nav id="mainImage" className="w3-sidebar w3-hide-medium w3-hide-small">
-                    <div className="bgimg" ></div>               
-                </nav>
+    return ( 
+        <>  
+        <form onSubmit={ifSubmit}> 
+            <nav id="mainImage" className="w3-sidebar w3-hide-medium w3-hide-small">
+                <div className="bgimg" ></div>               
+            </nav>
 
-                <div id="headDec">
+            <div id="headDec">
                 <header className="w3-container w3-center" id="home">
                     <h1 className="w3-jumbo">
                         <b>O-N Chat</b>
                     </h1>
                     <p> Fast, Simple and Secure Messaging.</p>
                 </header>
-                </div>
+            </div>
 
 
-                <div container id = "loginPageHeadline">
-                    <label>
-                        <div className ="w3-light-grey w3-container w3-center w3-large" > 
-                            Login
-                        </div>            
-                    </label>
-                </div>
+            <div container id = "loginPageHeadline">
+                <label>
+                    <div className ="w3-light-grey w3-container w3-center w3-large" > 
+                        Login
+                    </div>            
+                </label>
+            </div>
 
                 
                 
-                <div id="loginInfo">
+            <div id="loginInfo">
                 <form >
                     <label>               
                         <input type="text" name="username" className='w3-container w3-xlarge' onChange={ifChange} placeholder='Username'  />
@@ -61,38 +62,38 @@ function LoginPage({db}) {
                 </form>
                 </div>
 
-                <div id="loginInfo">
+            <div id="loginInfo">
                 <form>
                     <label>               
                         <input type="password" name="password" className='w3-container w3-xlarge' onChange={ifChange} placeholder='Password'  />
                     </label>
                 </form>
-                </div>
+            </div>
                 
                 
 
-                <p>
-                    <button className='w3-button w3-light-grey w3-padding-large' type='submit' id='button1'>
-                        Sign In
-                    </button>
-                    <button className='w3-button w3-center w3-light-grey w3-padding-large' type='submit' id='button2'>
-                        <Link to="/SignUpPage">Sign Up</Link>
-                    </button>
-                </p>
+            <p>
+                <button className='w3-button w3-center w3-light-grey w3-padding-large' type='submit' id='button1'>
+                    <Link to="/SignUpPage">Sign Up</Link>
+                </button>
+                <button className='w3-button w3-light-grey w3-padding-large' type='submit' id='button2'>
+                    Sign In
+                </button>
+            </p>
                 
-                <p>
-                    <footer
-                        id="poweredBy"
-                        className=" w3-container w3-padding-64 w3-light-grey w3-center w3-opacity w3-xlarge ">
-                        <p className="w3-medium">
-                            Powered by <a>Or Nasri &amp; Noa Benita</a>
-                        </p>
-                    </footer>
-                </p>
-            </form>
+            <p>
+                <footer
+                    id="poweredBy"
+                    className=" w3-container w3-padding-64 w3-light-grey w3-center w3-opacity w3-xlarge ">
+                    <p className="w3-medium">
+                        Powered by <a>Or Nasri &amp; Noa Benita</a>
+                    </p>
+                </footer>
+            </p>
+        </form>
                     
-            </>
-        );
+        </>
+    );
     
 }
 export default LoginPage;
