7fe27ac412936eae1aef37f599a8f87c450b42f0
noabenita <noaben2512@gmail.com>
2022-04-21 01:26:34 +0300

    update

--- src/chatPage/Message.js
+++ src/chatPage/Message.js
@@ -1,30 +1,35 @@
+import './Message.css';
 
 function Message({data, time, flag}){
     
     if (flag){
         console.log('right')
         return(
-         <li className="clearfix">
+         
+         <ul className="clearfix">
              <div className="message-data">
                  <span className="message-data-time"> <p>{time}</p> </span>
              </div>
              <div className="message my-message">
                     {" "} {data} {" "}
             </div>
-         </li>
+         </ul>
+        
         );
     } else {
         console.log('left')
 
         return(
-            <li className="clearfix">
-             <div className="message-data text-right">
+            <ul className='message-data-righ'>
+            <ul className="clearfix">
+             <div className=" text-right">
                  <span className="message-data-time-right"> <p>{time}</p> </span>
              </div>
              <div className="message other-message float-right">
                     {" "} {data} {" "}
             </div>
-         </li>
+            </ul>
+            </ul>
         );    
     }
 }
--- src/chatPage/RightSide.js
+++ src/chatPage/RightSide.js
@@ -1,20 +1,29 @@
 import React from "react";
 import './RightSide.css';
+import Message from "./Message";
 
 function RightSide({current,user,chat, setChat}){
     const[msg,setMsg] = React.useState("")
 
-    function submit(e){
-        chat.push({data:msg,time:new Date(),flag:false});
-        console.log({chat});
+    // insert to chat array of contact chat
+    function submit(event, data){
+    // check that msg is not empty message
+    const now = new Date();
+    const time = now.getHours() + ':' + now.getMinutes();
+        if(msg!=""){
+          chat.push({data:data,time:(time), flag:false});
+        }      
         setChat(chat);
         setMsg("");
         document.getElementById("msg").value = ("");
     }
     function ifChange(e) {
         setMsg(e.target.value)
-        console.log(e.target.value)
     }
+
+    const messageList = chat.map((message)=> {
+        return <Message data ={message.data} time ={message.time} flag={message.flag}/>
+      });
     return(
         <>
         <div className="boxo w3-container first-row">
@@ -23,24 +32,52 @@ function RightSide({current,user,chat, setChat}){
                 alt="avatar"/> {user.contact}
         </div>
 
-
-        <div className="chat-messeges" >
-        <ul className="friends-list">
-         {chat.map((message)=>
-             <li className="clearfix">
-                    {message.data}
-                    </li>    
-        )}  
-        </ul>
+        <div className="second-row chat-messeges">
+            <ul className="friends-list">
+                {chat.map((message)=>
+                <li className="clearfix ">
+                    <Message data = {message.data} time={message.time} flag={message.flag}/>
+                </li>)}  
+            </ul>
         </div>
-        <div className="send-text ">
-        <input className="text-line" type="input" placeholder="Enter your text here ..." 
-        id = "msg" name ="msg" value = {msg.msg} onChange={ifChange}>
 
-        </input>
+        <div className="third-row">   
+                 {console.log('done with array')}
+            
+        <th className="input-text send-text ">
+           <input type="text" className="text-line form-control" name="msg" id="msg" 
+           placeholder="Enter your message here" onChange={ifChange} value={msg.msg}></input>
+        </th>
+
+        <button onClick={(event, data) => submit(event, msg)} 
+        type="button" className="send-button butn btn btn-light"> send </button>
         </div>
-        <button className="send-button" type="button" onClick={submit} >send</button>
-        
+
+        <div class="dropdown">
+                <button class="btn btn-secondary fa fa-paperclip w3-xlarge" type="button" id="dropdownMenuButton" 
+                  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                </button>
+                <div className="dropdown-menu w3-light-grey" aria-labelledby="dropdownMenuButton">            
+                  <button  className='messegeTypes' >
+                    <input type="file" id="actual-btn" hidden/>
+                    <label className="fa fa-picture-o icons1 w3-large" for="actual-btn" ></label>
+                  </button>
+                
+                  <button  className='messegeTypes' >
+                    <input type="file" id="actual-btn" hidden/>
+                    <label className="fa fa-video-camera icons w3-large" for="actual-btn"></label>
+                  </button>
+
+                  <button 
+                  className="messegeTypesMic fa fa-microphone icons w3-large" data-toggle="modal" 
+                        // for="actual-btn" data-target="#exampleModal" onClick={recordFunc}*/
+                        >
+                  </button>
+
+                
+                </div>  
+            </div>
+
         </>
     );
 
