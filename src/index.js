29368061aa8c8285c434d620d3e601bb17fb991d
OrNasri <ornasri5@gmail.com>
2022-04-20 12:23:06 +0300

    all changes to new

--- src/index.js
+++ src/index.js
@@ -1,92 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import ChatPage from './chatPage/ChatPage';
-import ContactChat from './ContactChat';
-import LoginPage from './loginPage/LoginPage';
-import reportWebVitals from './reportWebVitals';
-import SignUpPage from './signUpPage/SignUpPage';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 
-
-
-// class User{
-
-//   constructor(username, nickname, img, password, chats){
-//     this.UserName = username;
-//     this.Nickname = nickname;
-//     this.Img = img;
-//     this.Password = password;
-//     this.Chats = chats;
-//   }
-
-// }
-
-// class Chat{
-//   constructor(){
-//     this.messages=[];
-//   }
-// }
-
-// class Message {
-//   constructor(time, data, flag){
-//     this.time = time;
-//     this.data = data;
-//     this.flag = flag; // 1 - if i send , 0- else
-//   }
-
-const currentUser = {nowOnline:null}; 
-
-const dataStructure= [
-  {UserName:'or',
-  NickName:'orush',
-  Img:"https://bootdey.com/img/Content/avatar/avatar3.png",
-  Password: 1, 
-  Chats:[
-    {contact: 'ron',
-    imgContact: "https://bootdey.com/img/Content/avatar/avatar2.png",
-    message:[
-      {data:"hey",time: '12:10',flag:true},
-      {data:"?",time: '12:15',flag:false},
-      {data:"what",time: "12:17",flag:false}]}, 
-    {contact :"hen",
-    imgContact: "https://bootdey.com/img/Content/avatar/avatar3.png",
-    message:[
-      {data:"how are u??",time: '14:10',flag:true},
-      {data:"♥",time: '14:11',flag:true},
-      {data:"fine, honey",time: "14:40",flag:false}]},
-    {contact :"noa",
-     imgContact: "https://bootdey.com/img/Content/avatar/avatar3.png",
-     message:[
-       {data:"how are u??",time: '14:10',flag:true},
-       {data:"♥",time: '14:11',flag:true},
-       {data:"fine, honey",time: "14:40",flag:false}]}]}];
-
-
-
-
-                     
-        
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-      <BrowserRouter>
-            <Routes>
-              <Route path="/" element ={<LoginPage db={dataStructure} current={currentUser}/>}>
-              </Route>
-              <Route path="/LoginPage" element ={<LoginPage db={dataStructure}  current={currentUser}/>}>
-              </Route>
-              <Route path="/SignUpPage" element ={<SignUpPage db={dataStructure}  current={currentUser}/>}>
-              </Route>
-              <Route path="/ChatPage" element ={<ChatPage db={dataStructure} current={currentUser}/>}>
-              </Route>
-              {/* <Route path="/ContactChat" element ={<ContactChat  db={dataStracture}  current={currentUser}/>}>
-              </Route> */}
-            </Routes>
-        </BrowserRouter>  
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
 
-reportWebVitals();
-// export default User
--- src/loginPage/LoginPage.js
+++ src/loginPage/LoginPage.js
@@ -1,32 +1,26 @@
-import {Link, useNavigate} from 'react-router-dom';
 import './LoginPage.css';
-import {useState} from 'react';
-import ChatPage from '../chatPage/ChatPage';
+import React from 'react';
 
-
-
-function LoginPage({db, current}) {
-    console.log(db);
-    const [usersArray,setUserArray] = useState({username:'', password:''});
-    var nav = useNavigate();
-    
-    function ifSubmit(x) {
-    
-        if(db.find((e) => e.UserName == usersArray.username && e.Password == usersArray.password)) {
-            current.nowOnline = usersArray.username;
-            for (var i =0; i <db.length; i++) {
-                 if(db[i].UserName == current.nowOnline) {
-                    current ={nowOnline:db[i]};                    
-                }  
+function LoginPage({db, setMode, setCurrent}) {
+    const [usersArray,setUserArray] = React.useState({username:'', password:''});          
+    function ifSubmited(e) {  
+        console.log('if submit im login');
+        for (var i =0; i <db.length; i++) {
+            if(db[i].UserName == usersArray.username && db[i].Password == usersArray.password){
+                setCurrent({
+                    UserName: db[i].UserName,
+                    NickName: db[i].NickName,
+                    Img: db[i].Img,
+                    Password: db[i].Password, 
+                    Chats: db[i].Chats
+                })
+                setMode(2);
+                return
+                } 
             }
-            nav("/ChatPage");
-        } else {
-            alert('username or password is incorrect');
-            x.preventDefault();
-        }
-       
-    }
-        
+        alert('username or password is incorrect'); 
+        e.preventDefault();
+    }      
     function ifChange(event) {
         const {name, value} = event.target;
         setUserArray({
@@ -34,67 +28,78 @@ function LoginPage({db, current}) {
             [name]:value
         })
     }
-        
+    function changeMode(){
+        setMode(1);
+        return
+    }
     return ( 
-        <>        
-            <nav id="mainImage" className="w3-sidebar w3-hide-medium w3-hide-small">
-                <div className="bgimg" ></div>               
-            </nav>
+        <>   
+        {/* <nav id="mainImage" className="w3-sidebar w3-hide-medium w3-hide-small">
+            <div className="bgimg" ></div>               
+        </nav> */}
 
-            <div id="headDec">
+            {/* <div id="headDec">
                 <header className="w3-container w3-center" id="home">
                     <h1 className="onChat w3-jumbo">
                         <b>O-N Chat</b>
                     </h1>
                     <p className="secHeadLine"> Fast, Simple and Secure Messaging.</p>
                 </header>
-            </div>
+            </div> */}
 
-            <div className = "loginPageHeadline w3-container w3-light-grey w3-center w3-opacity">
-                <label>
+            {/* <div className = "loginPageHeadline w3-container w3-light-grey w3-center w3-opacity"> */}
+                {/* <label> */}
                     <div className ="loginText w3-light-grey w3-container w3-center w3-large" > 
                         Login
                     </div>            
-                </label>
-            </div>        
+                {/* </label> */}
+            {/* </div>         */}
                 
             <div id="loginInfo">
                 <form >
                     <label>               
-                        <input type="text" name="username" className='w3-container w3-xlarge' onChange={ifChange} placeholder='Username'  />
+                        <input type="text" name="username" className='w3-container w3-xlarge' 
+                        onChange={ifChange} placeholder='Username'  />
                     </label>
-                </form>
-                </div>
 
-            <div id="loginInfo">
-                <form>
+
+            {/* <div id="loginInfo"> */}
                     <label>      
-                        <input type="password" name="password" className='w3-container w3-xlarge' onChange={ifChange} placeholder='Password'  />
+                        <input type="password" name="password" className='w3-container w3-xlarge'
+                         onChange={ifChange} placeholder='Password'  />
                     </label>
-                </form>
-            </div>
+                {/* </form> */}
                 
-            <form onSubmit={ifSubmit}> 
-                <button className='w3-button w3-center w3-light-grey w3-padding-large' id='button2'>
+            
+            
+            {/* <input id= 'button2' value="Sign-In" type="radio" name="tab" className="sign-in" 
+                 onClick={ifSubmited}> </input>
+                    <label htmlFor='button2' className='tab'> Sign In</label> */}
+               
+            <form > 
+                <button className='w3-button w3-center w3-light-grey w3-padding-large' 
+              onClick={ifSubmited}  id='button2'>
                     Sign In
                 </button>   
             </form>
 
-            <p >
-                <button className='w3-button w3-center w3-light-grey w3-padding-large' id='button1'>
-                    <Link to="/SignUpPage">Sign Up</Link>
-                </button>
-            </p>
-           
+            {/* <p > */}
+                <button id='button1' value="Sign-Up" type="radio" name="tab" className="sign-up" 
+                 onClick={changeMode}> Sign Up</button>
+                     {/* <label htmlFor='button1' className='tab'> Sign Up</label> */}
+                
+            {/* </p> */}
+           </form>
+           </div>
             <footer
               className="poweredBy w3-container w3-padding-64 w3-light-grey w3-center w3-opacity w3-xlarge ">
               <p className="w3-medium">
               Powered by <a>Or Nasri &amp; Noa Benita</a>
               </p>
             {/* End footer */}
-          </footer>
+           </footer>
            
-        </>
+         </>
     );
     
 }
