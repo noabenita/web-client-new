b5b57d8107ed42be417ac85d2d2d9e8bafac73b0
noabenita <noaben2512@gmail.com>
2022-04-21 02:10:04 +0300

    update last

--- src/chatPage/Message.js
+++ src/chatPage/Message.js
@@ -5,7 +5,7 @@ function Message({data, time, flag}){
     if (flag){
         console.log('right')
         return(
-         
+            <ul className='message-data-left col1'>
          <ul className="clearfix">
              <div className="message-data">
                  <span className="message-data-time"> <p>{time}</p> </span>
@@ -14,13 +14,13 @@ function Message({data, time, flag}){
                     {" "} {data} {" "}
             </div>
          </ul>
-        
+         </ul>
         );
     } else {
         console.log('left')
 
         return(
-            <ul className='message-data-righ'>
+            <ul className='message-data-righ col2'>
             <ul className="clearfix">
              <div className=" text-right">
                  <span className="message-data-time-right"> <p>{time}</p> </span>
